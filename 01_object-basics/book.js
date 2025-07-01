ceaab9e919d9914995ab0a108fe84c38cedda6e9
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-01 19:48:33 +0800

    01_object-basics: Created the __proto__ then check the prototype by using the Object.getPrototyOf() this will return true if the statement has a prototype.

--- 01_object-basics/book.js
+++ 01_object-basics/book.js
@@ -11,3 +11,17 @@ function Book(title, author, pages, read) {
 const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", false);
 
 console.log(theHobbit.info());
+
+//__proto__ Object ==============================>
+function Player(name, team, sport) {
+  this.name = name;
+  this.team = team;
+  this.sport = sport;
+  return `Hi! I'm ${this.name}, I have a team called ${this.team}  we play ${this.sport}`;
+}
+
+const player1 = new Player();
+const player2 = new Player();
+
+console.log(Object.getPrototypeOf(player1) === Player.prototype);
+console.log(Object.getPrototypeOf(player2) === Player.prototype);
