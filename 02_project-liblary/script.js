4dfab61927a324ca2ca78a265b204f28bad1f8df
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-02 20:17:58 +0800

    02)project-library: Click the Add Book the modal will show, at the same time when the close button is click the modal will be hidden

--- 02_project-liblary/script.js
+++ 02_project-liblary/script.js
@@ -1,3 +1,22 @@
+const addButton = document.querySelector("#addButton");
+
+const modal = document.querySelector(".modal");
+const mainContainer = document.querySelector(".container");
+const close = document.querySelector(".times");
+
+addButton.addEventListener("click", showModal);
+close.addEventListener("click", closeModal);
+
+function showModal() {
+  modal.classList.remove("hidden");
+  mainContainer.classList.add("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  mainContainer.classList.remove("hidden");
+}
+
 const myLibrary = [];
 
 function Book(title) {
