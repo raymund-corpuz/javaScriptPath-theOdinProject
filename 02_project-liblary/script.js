cac0d2c6c0236e1facc44b582f9913e30eb54601
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-02 22:07:01 +0800

    02_project-library: Get all the inputs from the Input Form then append them to the parent node using the forEach function

--- 02_project-liblary/script.js
+++ 02_project-liblary/script.js
@@ -1,9 +1,10 @@
+//----------------- GUI ---------------------------
 const addButton = document.querySelector("#addButton");
-
 const modal = document.querySelector(".modal");
 const mainContainer = document.querySelector(".container");
 const close = document.querySelector(".times");
 
+//------------------ SHOW / HIDE Modal ----------------
 addButton.addEventListener("click", showModal);
 close.addEventListener("click", closeModal);
 
@@ -17,25 +18,56 @@ function closeModal() {
   mainContainer.classList.remove("hidden");
 }
 
+//------------------ ADD BOOK FORM -----------------
+
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const date = document.querySelector("#date");
+const bookForm = document.querySelector(".bookForm");
+const listItem = document.querySelector(".list-item");
+
+bookForm.addEventListener("submit", saveForm);
+
+let bookData = {};
+
+function saveForm(e) {
+  e.preventDefault();
+  const bookTitle = title.value.trim();
+  const bookAuthor = author.value.trim();
+  const bookPages = pages.value.trim();
+
+  const newBookData = new Book(bookTitle, bookAuthor, bookPages);
+
+  addBookToLibrary(newBookData);
+
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+}
+
 const myLibrary = [];
 
-function Book(title) {
+function Book(title, author, pages) {
   this.title = title;
+  this.author = author;
+  this.pages = pages;
   this.id = Date.now();
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
-  return myLibrary;
+
+  myLibrary.forEach((book) => {
+    const listTitle = document.createElement("li");
+    listTitle.textContent = `Title: ${book.title}`;
+    listItem.appendChild(listTitle);
+    const spanAuthor = document.querySelector("span");
+    spanAuthor.textContent = `Author: ${book.author}`;
+    listTitle.appendChild(spanAuthor);
+    const spanDate = document.querySelector("span");
+    spanDate.textContent = `Pages: ${book.pages}`;
+    listTitle.appendChild(spanDate);
+  });
+  closeModal();
 }
-
-const harry = new Book("Harry");
-const potter = new Book("Potter");
-
-console.log(addBookToLibrary(harry));
-console.log(addBookToLibrary(potter));
-console.log(myLibrary[0]);
-
-myLibrary.forEach((book) => {
-  console.log(book.title);
-});
