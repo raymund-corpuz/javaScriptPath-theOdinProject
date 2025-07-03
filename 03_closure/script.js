07f120e8bd090dd8ca49cb68766a7f02025aaac3
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-03 19:40:49 +0800

    03_closure : Implementing Closure by making a banking system.

--- 03_closure/script.js
+++ 03_closure/script.js
@@ -10,3 +10,29 @@ function makeFunc() {
 
 const myFunc = makeFunc();
 myFunc();
+
+// CLOSURE IN DEPT DETAILS ================>
+
+function createAccount(account) {
+  let balance = account;
+
+  return {
+    deposit: (ammount) => {
+      balance = ammount + balance;
+      console.log(`You deposite ${ammount}, your total balance ${balance}`);
+    },
+
+    withdraw: (ammount) => {
+      if (ammount > balance) {
+        console.log("Insuficient balance");
+      } else {
+        balance = balance - ammount;
+        console.log(`You withdraw ${ammount}, your total balance ${balance}`);
+      }
+    },
+  };
+}
+const myAccount = createAccount(1000);
+myAccount.deposit(200);
+myAccount.withdraw(500);
+myAccount.withdraw(10000);
