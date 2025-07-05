b5e7f6b883f9e4a71bb3a1bf84fe5d54c52d95f4
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-05 08:09:26 +0800

    05_classes: Accessor and descriptor;

--- 05_classes/script.js
+++ 05_classes/script.js
@@ -14,3 +14,26 @@ let userGet = {
 userGet.fullName = "Alice Cooper";
 console.log(userGet.name);
 console.log(userGet.surname);
+console.log(userGet.fullName);
+console.log((userGet.fullName = "Raymund Corpuz"));
+console.log(userGet.name);
+console.log(userGet.surname);
+
+//Accessor Descriptors ===========================================>
+let userSet = {
+  name: "John",
+  surname: "Smith",
+};
+
+Object.defineProperty(userSet, "fullName", {
+  get() {
+    return `${this.name} ${this.surname}`;
+  },
+
+  set(value) {
+    [this.name, this.surname] = value.split(" ");
+  },
+});
+
+console.log(userSet.fullName);
+for (let key in userSet) console.log(key);
