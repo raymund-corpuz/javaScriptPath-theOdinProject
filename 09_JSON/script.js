1fc79db932bf0d0bba2ae30b2dafb56a9245afb4
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-09 08:27:30 +0800

    09_JSON : Initially the JSON format is a string we need to convert it using JSON.parse().

--- 09_JSON/script.js
+++ 09_JSON/script.js
@@ -38,3 +38,40 @@ const superHeroes = {
 
 console.log(superHeroes.homeTown);
 console.log(superHeroes.members[1].powers[2]);
+
+const arrayAsJson = [
+  {
+    name: "Molecule Man",
+    age: 29,
+    secretIdentity: "Dan Jukes",
+    powers: ["Radiation resistance", "Turning tiny", "Radiation blast"],
+  },
+  {
+    name: "Madame Uppercut",
+    age: 39,
+    secretIdentity: "Jane Wilson",
+    powers: ["Million tonne punch", "Damage resistance", "Superhuman reflexes"],
+  },
+];
+
+console.log(arrayAsJson[0].powers[0]);
+
+async function populate() {
+  const requestURL =
+    "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const superHeroes = await response.json();
+
+  populateHeader(superHeroes);
+  populateHeroes(superHeroes);
+}
+
+const jsonString = '{"name": "Raymund", "age": 25}';
+
+console.log(typeof jsonString);
+const obj = JSON.parse(jsonString);
+
+console.log(obj);
+console.log(typeof obj);
