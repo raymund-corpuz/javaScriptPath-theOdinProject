0340bb3e5062127cf2134b8dbf5f38730763a647
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-10 20:55:25 +0800

    10_todo-list: Add task functionality done.

---
+++ 10_todo-list/script.js
@@ -0,0 +1,43 @@
+const taskInputDOM = document.querySelector(".task-input");
+const taskButtonDOM = document.querySelector(".task-button");
+const taskListUl = document.querySelector(".task-list");
+
+class TaskItem {
+  constructor(task) {
+    this.task = task;
+    this.isComplete = false;
+    this.taskId = Date.now();
+  }
+
+  getData() {
+    return `Task ${this.task}, Completed: ${task.isComplete}, Id: ${this.taskId}`;
+  }
+}
+
+taskButtonDOM.addEventListener("click", createTask);
+
+function createTask(e) {
+  e.preventDefault();
+  const myNewTask = taskInputDOM.value;
+  const newTask = new TaskItem(myNewTask);
+  console.log(newTask);
+  renderTaskInterface(newTask);
+}
+
+function renderTaskInterface(task) {
+  const taskItemDOM = document.createElement("li");
+  const taskDelete = document.createElement("span");
+
+  taskItemDOM.classList.add("task-item");
+
+  //props
+  taskItemDOM.innerText = task.task;
+  taskDelete.innerHTML = `&times;`;
+
+  //append
+  taskListUl.appendChild(taskItemDOM);
+  taskItemDOM.appendChild(taskDelete);
+
+  //reset input
+  taskInputDOM.value = "";
+}
