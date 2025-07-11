769c673029597bf81d2bcc6b56ff1194f82f8c46
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-11 09:09:52 +0800

    10_todo-list: Save to Local Storage

--- 10_todo-list/final-todo-list/index.js
+++ 10_todo-list/final-todo-list/index.js
@@ -4,26 +4,35 @@ const searchTask = document.querySelector(".search-task");
 const addTaskButton = document.querySelector(".add-button");
 const outputContainer = document.querySelector(".output-section");
 //input-section====================================================>
-const titleInput = document.querySelector(".title-input");
-const descriptionInput = document.querySelector(".description-input");
-const dateInput = document.querySelector(".date-input");
-const priorityInput = document.querySelector(".priority-input");
-const saveButton = document.querySelector(".save");
-const cancelButton = document.querySelector(".cancel");
 
-//Initialization =========================================>
-const myTaskListArray = [];
 const myProjectListArray = [];
+
+//localStorage  Initialization ==============================>
+
+let myTaskListArray = getItemFromLocalStorage();
+
+function getItemFromLocalStorage() {
+  const key = localStorage.getItem("todo") || "[]";
+  return JSON.parse(key);
+}
+
+function setMyTaskListArray(items) {
+  const myTaskJSON = JSON.stringify(items);
+
+  localStorage.setItem("todo", myTaskJSON);
+}
+
+console.log(myTaskListArray);
 
 //Class Declaration ====================================>
 
 class CreateTask {
-  constructor(title, description, dueDate) {
+  constructor(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.id = Date.now();
-    this.priority = false;
+    this.priority = priority;
   }
   getCreateTask() {
     return `Title: ${this.title}, Description: ${this.description}, Id: ${this.id}, Due-date: ${this.dueDate}, Priority: ${this.priority}`;
@@ -43,10 +52,64 @@ class CreateProject {
 
 // Initialize Class =========================================>
 
-const myNewTask = new CreateTask("Hello", "Des", "");
-console.log(myNewTask);
-
 const myNewProject = new CreateProject("ProjectName");
 console.log(myNewProject);
 
 //UI ==================================================>
+const titleInput = document.querySelector(".title-input");
+const descriptionInput = document.querySelector(".description-input");
+const dateInput = document.querySelector(".date-input");
+const priorityInput = document.querySelector(".priority-input");
+const saveButton = document.querySelector(".save");
+const cancelButton = document.querySelector(".cancel");
+
+saveButton.addEventListener("click", handleSubmit);
+cancelButton.addEventListener("click", cancelSubmit);
+
+//Create new Task ========================================>
+function handleSubmit(e) {
+  e.preventDefault();
+  //get value
+  const titleSave = titleInput.value.trim();
+  const descriptionSave = descriptionInput.value.trim();
+  const dateSave = dateInput.value;
+  const prioritySave = priorityInput.value;
+
+  if (!titleSave || !descriptionSave || !dateSave || !prioritySave) {
+    alert("Complete Info");
+    return;
+  }
+
+  const myNewTask = new CreateTask(
+    titleSave,
+    descriptionSave,
+    dateSave,
+    prioritySave
+  );
+  console.log(myNewTask);
+
+  //empty input
+  titleInput.value = "";
+  descriptionInput.value = "";
+  dateInput.value = "";
+  priorityInput.value = "";
+
+  //save to localStorage
+  //   setMyTaskListArray(myNewTask);
+
+  //render UI
+  renderTaskly(myNewTask);
+}
+
+function cancelSubmit() {
+  console.log("cancel");
+}
+
+//render Taskly ======================>
+function renderTaskly(task) {
+  const taskItem = document.createElement("div");
+  taskItem.innerText = task.title;
+  outputContainer.appendChild(taskItem);
+
+  console.log(task);
+}
