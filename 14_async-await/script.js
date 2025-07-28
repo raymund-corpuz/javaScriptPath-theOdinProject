fca3fbd47bd0f9c384c098d113ba7a256c242e6d
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-28 20:56:54 +0800

    14_async-await: const promise is use to demonstrate the fetch func then the catch method.

--- 14_async-await/script.js
+++ 14_async-await/script.js
@@ -70,6 +70,13 @@ console.log("🥪 Synchronous 4");
 
 const promise = fetch("https://jsonplaceholder.typicode.com/todos/1");
 
-promise.then((res) => res.json()).then((user) => console.log("😊", user.title));
+promise
+  .then((res) => res.json())
+  .then((user) => {
+    throw new Error("uh oh");
+    return user;
+  })
+  .then((user) => console.log("😊", user.title))
+  .catch((err) => console.error("😭", err));
 
 console.log("🥪 Synchronous");
