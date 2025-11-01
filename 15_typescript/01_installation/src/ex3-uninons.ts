4ed24dd17013c6231477ec2752f8138e8484aa90
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-02 07:50:27 +0800

    15_typescript: Creating union types and narrowing.

---
+++ 15_typescript/01_installation/src/ex3-uninons.ts
@@ -0,0 +1,20 @@
+function getLength(value: string | string[]) {
+  if (typeof value === "string") {
+    return value.length;
+  }
+  return value.length;
+}
+
+console.log(getLength("Typescript"));
+console.log(getLength(["Type", "Script"]));
+
+function getLengths(value: string | string[]) {
+  if (typeof value === "string") {
+    return value.length;
+  }
+
+  return value.length;
+}
+
+console.log(getLengths("Typescript"));
+console.log(getLengths(["The", "quick", "brown"]));
