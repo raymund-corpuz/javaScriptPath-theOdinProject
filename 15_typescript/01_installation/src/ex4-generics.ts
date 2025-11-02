b653e57d28c851229dca46e11c232f88dd213bce
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-02 08:01:12 +0800

    15_typescript: Making a generic function with array transformation.

---
+++ 15_typescript/01_installation/src/ex4-generics.ts
@@ -0,0 +1,25 @@
+function identity<T>(value: T): T {
+  return value;
+}
+
+console.log(identity<string>("Hello"));
+console.log(identity<number>(100));
+
+function wrapInArray<T>(item: T): T[] {
+  return [item];
+}
+
+console.log(wrapInArray("TS Rocks"));
+
+function identification<T>(value: T): T {
+  return value;
+}
+
+console.log(identification<string>("Hello"));
+console.log(identification<number>(100));
+
+function myArray<T>(item: T): T[] {
+  return [item];
+}
+
+console.log(myArray("Type"));
