2fc2f12c7eca3f18bb36071dd3c00ae423a91f5a
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-03 07:20:12 +0800

    15_typescript: Re-cap doing again all the exercise as a recap with the lessons.

--- 15_typescript/01_installation/src/ex1-types.ts
+++ 15_typescript/01_installation/src/ex1-types.ts
@@ -1,11 +1,23 @@
-let greeting = "Hello TypeScript";
+// let greeting = "Hello TypeScript";
+
+// let age: number = 25;
+
+// // age = 'twenty-five';
+
+// function add(a: number, b: number) {
+//   return a + b;
+// }
+
+// console.log(add(5, 10));
+
+let greeting = "hello Typescript";
 
 let age: number = 25;
 
-// age = 'twenty-five';
+age = "twenty-five";
 
 function add(a: number, b: number) {
   return a + b;
 }
 
-console.log(add(5, 10));
+console.log(add(15, 19));
