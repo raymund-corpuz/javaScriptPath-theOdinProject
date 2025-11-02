2f1f22bbf23ca460b1142a945eb459d5b8dd5fca
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-03 07:20:29 +0800

    15_typescript: Re-cap doing again all the exercise as a recap with the lessons.

--- 15_typescript/01_installation/src/ex2-interfaces.ts
+++ 15_typescript/01_installation/src/ex2-interfaces.ts
@@ -1,20 +1,42 @@
+// // interface User {
+// //   id: number;
+// //   name: string;
+// //   email: string;
+// //   isAdmin?: boolean; //optional
+// // }
+
+// // //create two user objects
+// // const user1: User = { id: 1, name: "Alice", email: "alice@email.com" };
+// // const user2: User = {
+// //   id: 2,
+// //   name: "Bob",
+// //   email: "bob@eamil.com",
+// //   isAdmin: true,
+// // };
+
+// // //create a function that prints user info
+// // function printUser(user: User) {
+// //   console.log(`${user.name} (${user.email})`);
+// // }
+
+// // printUser(user1);
+// // printUser(user2);
+
 // interface User {
 //   id: number;
 //   name: string;
 //   email: string;
-//   isAdmin?: boolean; //optional
+//   isAdmin: boolean; //optional
 // }
 
-// //create two user objects
 // const user1: User = { id: 1, name: "Alice", email: "alice@email.com" };
 // const user2: User = {
 //   id: 2,
 //   name: "Bob",
-//   email: "bob@eamil.com",
+//   email: "bob@email.com",
 //   isAdmin: true,
 // };
 
-// //create a function that prints user info
 // function printUser(user: User) {
 //   console.log(`${user.name} (${user.email})`);
 // }
@@ -26,20 +48,18 @@ interface User {
   id: number;
   name: string;
   email: string;
-  isAdmin: boolean; //optional
+  isAdmin: boolean;
 }
 
-const user1: User = { id: 1, name: "Alice", email: "alice@email.com" };
-const user2: User = {
-  id: 2,
-  name: "Bob",
-  email: "bob@email.com",
+const user: User = {
+  id: 1,
+  name: "Alice",
+  email: "alice@email.com",
   isAdmin: true,
 };
 
-function printUser(user: User) {
-  console.log(`${user.name} (${user.email})`);
+function addUser(user: User) {
+  return `${user.name} (${user.email})`;
 }
 
-printUser(user1);
-printUser(user2);
+console.log(addUser(user));
--- 15_typescript/01_installation/src/ex3-uninons.ts
+++ 15_typescript/01_installation/src/ex3-uninons.ts
@@ -1,20 +1,29 @@
+// function getLength(value: string | string[]) {
+//   if (typeof value === "string") {
+//     return value.length;
+//   }
+//   return value.length;
+// }
+
+// console.log(getLength("Typescript"));
+// console.log(getLength(["Type", "Script"]));
+
+// function getLengths(value: string | string[]) {
+//   if (typeof value === "string") {
+//     return value.length;
+//   }
+
+//   return value.length;
+// }
+
+// console.log(getLengths("Typescript"));
+// console.log(getLengths(["The", "quick", "brown"]));
+
 function getLength(value: string | string[]) {
   if (typeof value === "string") {
     return value.length;
   }
   return value.length;
 }
-
-console.log(getLength("Typescript"));
-console.log(getLength(["Type", "Script"]));
-
-function getLengths(value: string | string[]) {
-  if (typeof value === "string") {
-    return value.length;
-  }
-
-  return value.length;
-}
-
-console.log(getLengths("Typescript"));
-console.log(getLengths(["The", "quick", "brown"]));
+console.log(getLength("TypeScript"));
+console.log(getLength(["type", "script"]));
